6b0530f6f677660693e8b9574e4459c06a1f0253
Ujin28 <melon4092@naver.com>
2022-07-18 18:53:29 +0900

    회원가입화면 수정

--- SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoSignup.java
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoSignup.java
@@ -1,9 +1,11 @@
 package com.sovoro;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,22 +33,151 @@ import java.util.Map;
 // 회원가입 액티비티
 public class SoVoRoSignup extends AppCompatActivity {
     private ActivitySovoroSignupBinding activitySovoroSignupBinding;
-
+    private RequestQueue queue;
     private final String SIGNUP_PATH="/signup";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sovoro_signup);
 
-        AppHelper.setRequestQueue(this);
+        activitySovoroSignupBinding.sovoroSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String URL = AppHelper.getURL(SIGNUP_PATH);
+                String userid = activitySovoroSignupBinding.sovoroSignupId.getText().toString();
+                String password = activitySovoroSignupBinding.sovorosignuppassword.getText().toString();
+                String nickname = activitySovoroSignupBinding.sovorosignupnickname.getText().toString();
 
 
-        final String URL = AppHelper.getURL(SIGNUP_PATH);
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
 
-        activitySovoroSignupBinding = ActivitySovoroSignupBinding.inflate(getLayoutInflater());
-        setContentView(activitySovoroSignupBinding.getRoot());
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Signup",response);
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "회원가입 완료", Toast.LENGTH_LONG).show();
 
-       /* private JsonObjectRequest getJsonObjectRequest() {
+                                Intent intent = new Intent(SoVoRoSignup.this, SoVoRoSignin.class);
+                                startActivity(intent);
+                                //finish();
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원가입 실패", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                };
+
+                Response.ErrorListener errorListener = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),"회원가입 처리 에러",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                };
+
+
+
+                RegisterRequest registerRequest = new RegisterRequest(URL, userid, password, nickname, responseListener, errorListener);
+                queue = Volley.newRequestQueue(SoVoRoSignup.this);
+                queue.add(registerRequest);
+                //AppHelper.getURL(SIGNUP_PATH),
+                // activitySovoroSignupBinding.sovoroSignupId.getText().toString(),
+                // activitySovoroSignupBinding.sovorosignuppassword.getText().toString(),
+                // activitySovoroSignupBinding.sovorosignupnickname.getText().toString(),
+                //responseListener
+                //);
+
+            }
+        });
+
+
+
+}
+
+    public class RegisterRequest extends StringRequest {
+        private Map<String, String> map;
+
+
+
+
+        //private String userid;
+        //private String password;
+        //private String nickname;
+
+        /*public RegisterRequest(String userid, String password, String nickname, String URL, Response.Listener<String> listener) {
+            super(Method.POST, URL, listener, null);
+            map = new HashMap<>();
+        }*/
+
+        public RegisterRequest(String URL, String userid, String password, String nickname, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+            super(Method.POST, URL, listener, errorListener);
+
+            map = new HashMap<>();
+            map.put("userid", userid);
+            map.put("password", password);
+            map.put("nickname", nickname);
+        }
+
+        /*public void insertIntoMap(String key, String value) {
+            map.put(key,value);
+        }*/
+        @Nullable
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            return map;
+        }
+
+
+
+    }
+
+
+}
+
+    /*public class RegisterRequest extends StringRequest {
+        private Map<String, String> map;
+
+        private String userid;
+        private String password;
+        private String nickname;
+
+        public RegisterRequest(String userid, String password, String URL, Response.Listener<String> listener) {
+            super(Method.POST, URL, listener, null);
+            map = new HashMap<>();
+        }
+
+        public RegisterRequest(String URL,String userid, String password, String nickname, Response.Listener<String> listener) {
+            super(Method.POST, URL, listener, null);
+
+            map = new HashMap<>();
+            map.put("userid", userid);
+            map.put("password", password);
+            map.put("nickname", nickname);
+        }
+        public void insertIntoMap(String key, String value) {
+            map.put(key,value);
+        }
+        @Nullable
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            return map;
+        }
+    }*/
+
+
+
+
+
+/* private JsonObjectRequest getJsonObjectRequest() {
             return new JsonObjectRequest(
                     Request.Method.POST,
                     URL,
@@ -109,57 +240,3 @@ public class SoVoRoSignup extends AppCompatActivity {
                 return params;
             }
     };*/
-
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    boolean success = jsonResponse.getBoolean("success");
-                    if (success) {
-                        Toast.makeText(getApplicationContext(), "회원가입 완료", Toast.LENGTH_LONG).show();
-
-                        Intent intent = new Intent(getApplicationContext(), SoVoRoSignin.class);
-                        startActivity(intent);
-                        //finish();
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "회원가입 실패", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-        };
-
-        RegisterRequest registerRequest = new RegisterRequest(activitySovoroSignupBinding.sovoroSignupId, activitySovoroSignupBinding.sovorosignuppassword, activitySovoroSignupBinding.sovorosignupnickname, reponseListener);
-        RequestQueue queue = Volley.newRequestQueue(SoVoRoSignup.this);
-        queue.add(registerRequest);
-
-
-    }
-
-    public class RegisterRequest extends StringRequest {
-        final static private String URL ="";
-        private Map<String, String> map;
-
-        public RegisterRequest(String userid, String password, String nickname, Response.Listener<String> listener) {
-            super(Method.POST, URL, listener, null);
-
-            map = new HashMap<>();
-            map.put("userid", userid);
-            map.put("password", password);
-            map.put("nickname", nickname);
-        }
-
-        @Override
-        protected Map<String, String> getMap() throws AuthFailureError{
-            return map;
-        }
-    }
-}
