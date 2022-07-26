15d7f8fff4757e839ec4e9408bf3bf169add1b68
201710787 <egnachio@naver.com>
2022-07-26 18:57:42 +0900

    message system

---
+++ SoVoRo-client/app/src/main/java/com/sovoro/UserInfo.java
@@ -0,0 +1,13 @@
+package com.sovoro;
+
+public class UserInfo {
+    public static String userId;
+    public static String nickname;
+    public static String password;
+
+    public UserInfo(String userId, String nickname, String password) {
+        this.userId=userId;
+        this.nickname=nickname;
+        this.password=password;
+    }
+}
